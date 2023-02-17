e1391106498f684eb328734b5670750fdf4bd3b8
Dylan Hays <twentygx@outlook.com>
2023-02-17 15:36:40 -0600

    commit tree 48013fff9a04b367c8bb4535ead3ffd18beae2cc

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (0, "Mr. Chocolate", 12, "M", "Black", "Laid-Back", 1);
+INSERT INTO bears VALUES (1, "Rowdy", 3, "F", "Brown", "Rowdy", 1);
+INSERT INTO bears VALUES (2, "Tabitha", 6, "F", "Brown", "Energetic", 1);
+INSERT INTO bears VALUES (3, "Sergeant Brown", 15, "M", "Black", "Loyal", 0);
+INSERT INTO bears VALUES (4, "Melissa", 7, "F", "Black", "Caring", 1);
+INSERT INTO bears VALUES (5, "Grinch", 14, "M", "Green", "Aggressive", 0);
+INSERT INTO bears VALUES (6, "Wendy", 1, "F", "Brown", "Caring", 1);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (7, "F", "White", "Easy-going", 1);
